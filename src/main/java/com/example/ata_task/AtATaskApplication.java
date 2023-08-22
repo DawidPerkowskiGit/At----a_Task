38fb2f98d5e98634c19de4da274ab99fbd4a81ba
DawidPerkowskiGit <dtperkowski@gmail.com>
2023-08-22 10:50:16 +0200

    Initial commit

---
+++ src/main/java/com/example/ata_task/AtATaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.ata_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtATaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtATaskApplication.class, args);
+    }
+
+}
